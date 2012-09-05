d3f719832465eb2a5aa694340b968220808cc6c2
Lucas Jandrew <fapiko@gmail.com>
2012-09-05 02:22:06 -0700

    Added on click listener to buttons

--- src/com/fapiko/faceworm/mobile/android/FacewormMobileAndroid.java
+++ src/com/fapiko/faceworm/mobile/android/FacewormMobileAndroid.java
@@ -6,15 +6,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FacewormMobileAndroid extends Activity {
 
 	public final String TAG = "FacewormMobileAndroid";
 
-	private Button playPauseButton;
-	private Button nextTrackButton;
-	private Button thumbsUpButton;
-	private Button thumbsDownButton;
+	private ImageButton playPauseButton;
+	private ImageButton nextTrackButton;
+	private ImageButton thumbsUpButton;
+	private ImageButton thumbsDownButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,14 +31,45 @@ public class FacewormMobileAndroid extends Activity {
 
 	public void addButtonListeners() {
 
-		playPauseButton = (Button) findViewById(R.id.playPauseButton);
-		playPauseButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Log.d(TAG, "Play Pressed");
-			}
-		});
 
+		playPauseButton = (ImageButton) this.findViewById(R.id.playPauseButton);
+		playPauseButton.setOnClickListener(new MyOnClickListener(this, MyOnClickListener.PLAYPAUSE));
+
+		nextTrackButton = (ImageButton) this.findViewById(R.id.nextTrackButton);
+		nextTrackButton.setOnClickListener(new MyOnClickListener(this, MyOnClickListener.NEXTTRACK));
+
+		thumbsUpButton = (ImageButton) this.findViewById(R.id.thumbsUpButton);
+		thumbsUpButton.setOnClickListener(new MyOnClickListener(this, MyOnClickListener.THUMBSUP));
+
+		thumbsDownButton = (ImageButton) this.findViewById(R.id.thumbsDownButton);
+		thumbsDownButton.setOnClickListener(new MyOnClickListener(this, MyOnClickListener.THUMBSDOWN));
+
+	}
+
+	private class MyOnClickListener implements View.OnClickListener {
+
+		private final static int PLAYPAUSE = 0;
+		private final static int NEXTTRACK = 1;
+		private final static int THUMBSUP = 2;
+		private final static int THUMBSDOWN = 3;
+
+		private int buttonId;
+
+		private FacewormMobileAndroid parent;
+
+		public MyOnClickListener(FacewormMobileAndroid parent, int buttonId) {
+
+			this.parent = parent;
+			this.buttonId = buttonId;
+
+		}
+
+		@Override
+		public void onClick(View view) {
+
+			Log.d(TAG, "Button pressed: " + buttonId);
+
+		}
 	}
 
 }
